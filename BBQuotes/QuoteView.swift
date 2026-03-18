e89bfa5bffa492e44fe531b48ed6f201b28295f5
Júlio Andherson <julioandherson1@gmail.com>
2026-03-17 22:24:14 -0300

    create action

--- BBQuotes/QuoteView.swift
+++ BBQuotes/QuoteView.swift
@@ -20,48 +20,61 @@ struct QuoteView: View {
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 
                 VStack {
-                    Spacer(minLength: 60)
-                    
-                    Text("\"\(vm.quote.quote)\"")
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.white)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .clipShape(.rect(cornerRadius: 25))
-                        .padding(.horizontal)
-                    
-                    ZStack(alignment: .bottom) {
-                        AsyncImage(url: vm.character.images[0]) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    VStack {
+                        Spacer(minLength: 60)
                         
-                        Text(vm.quote.character)
-                            .foregroundStyle(.white)
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
+                        switch vm.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text("\"\(vm.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: vm.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                                
+                                Text(vm.quote.character)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                            }
+                            .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case .failed(let error):
+                            Text(error.localizedDescription)
+                        }
+                        
+                        Spacer()
                     }
-                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-                    .clipShape(.rect(cornerRadius: 50))
-                    
-                    Spacer()
                     
                     Button {
-                        
+                        Task {
+                            await vm.getData(for: show)
+                        }
                     } label: {
                         Text("Get Random Quote")
                             .font(.title)
                             .foregroundStyle(.white)
                             .padding()
-                            .background(.breakingBadGreen)
+                            .background(Color("\(show.replacingOccurrences(of: " ", with: ""))Button"))
                             .clipShape(.rect(cornerRadius: 7))
-                            .shadow(color: .breakingBadYellow, radius: 2)
+                            .shadow(color: Color("\(show.replacingOccurrences(of: " ", with: ""))Shadow"), radius: 2)
                     }
                     Spacer(minLength: 95)
                 }
@@ -72,6 +85,9 @@ struct QuoteView: View {
     }
 }
 
+//#Preview {
+//    QuoteView(show: "Breaking Bad").preferredColorScheme(.dark)
+//}
 #Preview {
-    QuoteView(show: "Breaking Bad").preferredColorScheme(.dark)
+    QuoteView(show: "Better Call Saul").preferredColorScheme(.dark)
 }
