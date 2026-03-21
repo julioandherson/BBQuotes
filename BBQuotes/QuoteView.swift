d6433e1be22e303b6f5b36df254ff54dd4301a37
Júlio Andherson <julioandherson1@gmail.com>
2026-03-20 23:59:00 -0300

    create character view

--- BBQuotes/QuoteView.swift
+++ BBQuotes/QuoteView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct QuoteView: View {
     let vm = ViewModel()
     let show: String
+    @State
+    var showCharacterInfo = false
     
     var body: some View {
         GeometryReader { geo in
@@ -56,6 +58,9 @@ struct QuoteView: View {
                             }
                             .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
                             .clipShape(.rect(cornerRadius: 50))
+                            .onTapGesture {
+                                showCharacterInfo.toggle()
+                            }
                         case .failed(let error):
                             Text(error.localizedDescription)
                         }
@@ -81,7 +86,11 @@ struct QuoteView: View {
                 .frame(width: geo.size.width, height: geo.size.height)
             }
             .frame(width: geo.size.width, height: geo.size.height)
-        }.ignoresSafeArea()
+        }
+        .ignoresSafeArea()
+        .sheet(isPresented: $showCharacterInfo) {
+            CharacterView(character: vm.character, show: show)
+        }
     }
 }
 
